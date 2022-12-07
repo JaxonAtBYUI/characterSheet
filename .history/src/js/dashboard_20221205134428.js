df53885625bab063eff159c6fb0868515268c62c
purnsup <purnsup90@gmail.com>
2022-12-07 13:45:06 -0700

    dashboard

---
+++ .history/src/js/dashboard_20221205134428.js
@@ -0,0 +1,45 @@
+import { loadHeaderFooter, loadTemplate } from "./utils.mjs";
+import supabase from "./SupaSetup.mjs"
+
+console.log(supabase);
+
+let { data: characters, error } = await supabase
+  .from('characters')
+  .select('*');
+const template = await loadTemplate("../partials/dashboard_item.html");
+console.log(characters.length);
+console.log(template);
+
+for(let i = 0; i <characters.length; i++){
+  const name = characters[i].name;
+  const cls = characters[i].class;
+  const level = characters[i].level;
+  const background= characters[i].background;
+  const ac = armor_class;
+  const initiative = characters[i].initiative;
+  const speed = character[i].speed;
+  // const firstItem =
+  // 
+  console.log(name);
+  renderDashboard(template, chracters[i])
+}
+function renderDashboard(template, item){
+  const items = document.querySelector('.dashBoard__items');
+  // const item = `
+  //     <div class="character__item">
+  //       <div class="name"><h3>${name}</h3></div>
+  //       <div class="character__info">
+  //       <span>${cls}</span><span>${level}</span><span>${background}</span><span>AC</span><span>Initiative</span><span>Speed</span><span>First Item</span>
+  //       </div>
+  //     </div>`;
+  template.querySelector(".name").innerHTML = item.name;
+  template.querySelector(".cls").innerHTML
+  template.querySelector(".lvl").innerHTML
+  template.querySelector(".bg").innerHTML
+  template.querySelector(".").innerHTML
+  template.querySelector(".").innerHTML
+  items.appendChild(item);
+
+}
+
+loadHeaderFooter();
